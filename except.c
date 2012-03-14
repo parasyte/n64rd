f95922f4b84e3bc756fe729fd1fa11e011a17a27
Jay Oster <parasyte@kodewerx.org>
2012-03-14 16:38:29 -0700

    Fix warnings in except.c

--- except.c
+++ except.c
@@ -1,8 +1,10 @@
 
+#include <setjmp.h>
+
 #include "except.h"
 
 
 /* Protected variables */
-jmp_buf _exception_env = { 0 };
+jmp_buf _exception_env = { { { 0 } } };
 Exception _exception_list[_EXCEPTION_LIST_SIZE] = { { 0 } };
 int _exception_stack = 0;
